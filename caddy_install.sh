83259475cbeeb8780647f8d2b1793b22bb5d5b05
sancdvs <2321310864@qq.com>
2020-04-05 22:27:11 +0800

    switch download for speed

--- caddy_install.sh
+++ caddy_install.sh
@@ -55,7 +55,7 @@ Download_caddy(){
 	elif [[ ${bit} == "i386" || ${bit} == "i686" ]]; then
 		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://github.com/caddyserver/caddy/releases/download/v1.0.4/caddy_v1.0.4_linux_386.tar.gz"
 	elif [[ ${bit} == "armv7l" ]]; then
-		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://github.com/caddyserver/caddy/releases/download/v1.0.4/caddy_v1.0.4_linux_arm7.tar.gz"
+		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://github.com/sancdvs/caddy_install/blob/master/caddy_v1.0.4_linux_arm7.tar.gz?raw=true"
 	else
 		echo -e "${Error_font_prefix}[错误]${Font_suffix} 不支持 [${bit}] ! 请向本站反馈[]中的名称，我会看看是否可以添加支持。" && exit 1
 	fi
